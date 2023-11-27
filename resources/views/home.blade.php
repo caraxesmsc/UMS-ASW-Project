e8f7ab25beb60b95c6e4ebd96b7c56b1f96618d9
caraxesmsc <mohamedongo21@gmail.com>
2023-11-27 02:27:57 +0200

    init card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,6 +17,16 @@
                     {{ __('You are logged in!') }}
                 </div>
             </div>
+
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                  <h5 class="card-title">Hossam Gamed Moot</h5>
+                  <h6 class="card-subtitle mb-2 text-body-secondary">Develop some trust</h6>
+                  <p class="card-text">bla bla</p>
+                  <a href="#" class="card-link">Card link</a>
+                  <a href="#" class="card-link">Another link</a>
+                </div>
+              </div>
         </div>
     </div>
 </div>
